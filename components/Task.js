21ebdd72757a6169c67916d5c01ec6ceadfa7505
tamaspalya <tamas.palya01@gmail.com>
2022-09-08 17:16:14 +0200

    TouchableOpacity changed to View. More styling added to task component.

--- components/Task.js
+++ components/Task.js
@@ -5,7 +5,7 @@ const Task = (props) => {
     return (
         <View style={styles.item}>
             <View style={styles.itemLeft}>
-                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <View style={styles.square}></View>
                 <Text style={styles.itemText}>{props.text}</Text>
             </View>
             <View style={styles.circular}></View>
@@ -32,12 +32,18 @@ const styles = StyleSheet.create({
         height: 24,
         backgroundColor: '#55BCF6',
         opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
     },
     itemText: {
-
+        maxWidth: '80%',
     },
     circular: {
-
+        width: 12,
+        height: 12,
+        borderColor: '#55BCF6',
+        borderWidth: 2,
+        borderRadius: 5,
     }
 });
 
